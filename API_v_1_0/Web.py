e6d9c52f12829799a94d26af2a212a8c9aad885c
Monstrofil <oshyshatskyi@cloudlinux.com>
2017-06-30 22:56:56 +0300

    Updated web skeleton

--- API_v_1_0/Web.py
+++ API_v_1_0/Web.py
@@ -1,16 +1,33 @@
 #!/usr/bin/python
 # coding=utf-8
 __author__ = "Aleksandr Shyshatsky"
+from typing import Callable
 
 
 class Web(object):
-    def getAllowedUrls(self, ):
+    def getAllowedUrls(self):
         pass
 
     def addAllowedUrl(self, encodedUrl):
+        """
+        Accepts encrypted url and adds it to whitelist;
+        Returns True if success, False otherwise
+        :type encodedUrl: str 
+        :rtype: bool 
+        """
         pass
 
     def openUrl(self, url):
+        pass
+
+    def openUrlAsync(self, url, callback, data):
+        """
+        Open url and get result in callback (async)
+        :type url: str 
+        :type callback: Callable[str]
+        :type data: dict
+        :rtype: None 
+        """
         pass
 
     def urlEncode(self, query, doseq=0):

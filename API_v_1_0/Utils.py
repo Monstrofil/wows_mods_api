fd87cc4b7f40924251113ad0b18fb094e6794b21
Oleksandr Shyshatsky <oshyshatskyi@cloudlinux.com>
2018-04-27 20:52:42 +0300

    implements some method in utils module

--- API_v_1_0/Utils.py
+++ API_v_1_0/Utils.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 # coding=utf-8
+import os
+import datetime
+import json
 
 __author__ = "Aleksandr Shyshatsky"
 
@@ -15,38 +18,40 @@ class Utils(object):
 
     @staticmethod
     def stat(path):
-        pass
+        return os.stat(path)
 
     @staticmethod
     def walk(top, topdown, followlinks):
-        pass
+        return os.walk(top, topdown, followlinks)
 
     @staticmethod
     def isFile(path):
-        pass
+        return os.path.isfile(path)
 
     @staticmethod
     def isDir(path):
-        pass
+        return os.path.isdir(path)
 
     @staticmethod
     def isPathExists(path):
-        pass
+        return os.path.exists(path)
 
     @staticmethod
     def timeNowUTC():
-        pass
+        return datetime.datetime.utcnow()
 
     @staticmethod
     def timeNow():
-        pass
+        return datetime.datetime.now()
 
     @staticmethod
-    def jsonEncode(obj, skipkeys=None, ensure_ascii=None, check_circular=None,
-                   allow_nan=None, indent=None, separators=None, encoding=None,
-                   default=None, sort_keys=None):
-        pass
+    def jsonEncode(obj, skipkeys=False, ensure_ascii=True, check_circular=True,
+                   allow_nan=True, cls=None, indent=None, separators=None,
+                   encoding='utf-8', default=None, sort_keys=False, **kw):
+        return json.dumps(obj, skipkeys, ensure_ascii, check_circular,
+                          allow_nan, cls, indent, separators, encoding,
+                          default, sort_keys, **kw)
 
     @staticmethod
     def jsonDecode(s, encoding='utf-8', parse_float=False, parse_int=False, parse_constant=False):
-        pass
+        return json.loads(s, encoding, parse_float, parse_int, parse_constant)
